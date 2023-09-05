057eb0a33ec4a7237d0edb906b28da7661c20c13
hhaldin <haldin@iastate.edu>
2023-09-04 19:26:15 -0500

    Add spring boot dependency to POM file

---
+++ Experiments/hhaldin/catAPI/src/main/java/com/example/catapi/tests/CatController.java
@@ -0,0 +1,12 @@
+package com.example.catapi.tests;
+
+import com.springframework.web.bind.annotation.GetMapping;
+import com.springframework.web.bind.annotation.PostMapping;
+import com.springframework.web.bind.annotation.PutMapping;
+import com.springframework.web.bind.annotation.RequestBody;
+import com.springframework.web.bind.annotation.DeleteMapping;
+import com.springframework.web.bind.annotation.RequestBody;
+import com.springframework.web.bind.annotation.RequestParam;
+import com.springframework.web.bind.annotation.RestController;
+public class CatController {
+}
