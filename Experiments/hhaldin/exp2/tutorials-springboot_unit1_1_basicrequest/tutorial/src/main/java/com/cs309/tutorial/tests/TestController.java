e05af9b73eab0e42efc4ce3a2375935befcd56ec
hhaldin <haldin@iastate.edu>
2023-09-04 20:28:33 -0500

    Read and write to the data.txt file

--- Experiments/hhaldin/exp2/tutorials-springboot_unit1_1_basicrequest/tutorial/src/main/java/com/cs309/tutorial/tests/TestController.java
+++ Experiments/hhaldin/exp2/tutorials-springboot_unit1_1_basicrequest/tutorial/src/main/java/com/cs309/tutorial/tests/TestController.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+
+/*
+	I'm using a basic text file to model a database in the following controller.
+	POST methods write to data.txt and GET retrieves all the data from it.
+ */
+
 @RestController
 public class TestController {
 	
@@ -16,19 +26,50 @@ public class TestController {
 
 	@GetMapping("/getTest")
 	public String getTest(@RequestParam(value = "username", defaultValue = "World") String message) {
-		return String.format("Hello, %s! You sent a get request with a parameter!", message);
+		String fileName = "data.txt";
+		StringBuilder fileContents = new StringBuilder();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+			String line;
+			while ((line = reader.readLine()) != null) {
+				fileContents.append(line).append("\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Failed to read file contents.";
+		}
+
+		return fileContents.toString();
 	}
 	
 	@PostMapping("/postTest1")
 	public String postTest1(@RequestParam(value = "username", defaultValue = "World") String message) {
-		//TODO
-		return String.format("Hello, %s! You sent a post request with a parameter!", message);
+		try {
+			FileWriter writer = new FileWriter("data.txt", true); // Append to the file
+			writer.write(message + "\n");
+			writer.close();
+			return String.format("Hello, %s! Your message has been saved to a file.", message);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Failed to save the message.";
+		}
 	}
 	
 	@PostMapping("/postTest2")
 	public String postTest2(@RequestBody TestData testData) {
-		//TODO
-		return String.format("Hello, %s! You sent a post request with a requestbody!", testData.getMessage());
+		// Store the data sent to this endpoint by writing it to a file
+		String message = testData.getMessage();
+
+		//Save the message to a file
+		try {
+			FileWriter writer = new FileWriter("data.txt", true); // true for appending to existing contents
+			writer.write(message + "\n");
+			writer.close();
+			return String.format("Hello, %s! You sent a post request with a requestbody!", testData.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Failed to save the message.";
+		}
 	}
 	
 	@DeleteMapping("/deleteTest")
