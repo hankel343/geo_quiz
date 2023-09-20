2a69238c5dc504eef94b2c6f141db04eb1c86898
Brayden Lamb <balamb@iastate.edu>
2023-09-20 10:25:28 -0500

    Basic Home Screen

---
+++ Experiments/balamb/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/GamescreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.geoquizfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamescreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamescreen);
+    }
+}
