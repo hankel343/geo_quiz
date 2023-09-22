1c84fa281bd4431e88a5375b8550cb53de057414
Brayden Lamb <balamb@iastate.edu>
2023-09-22 10:38:29 -0500

    Added base Capital and Flag game Screens

---
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalGame.java
@@ -0,0 +1,14 @@
+package com.example.geoquizfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CapitalGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capital_game);
+    }
+}
