228cb89db8d5fe51b82625ca9df89539b57134c7
Brayden Lamb <balamb@iastate.edu>
2023-10-02 12:29:29 -0500

    Base Frontend Done

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalGame.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalGame.java
@@ -1,15 +1,17 @@
 package com.example.geoquizfrontend;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.view.View;
-import android.widget.*;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CapitalGame extends AppCompatActivity {
     TextView CapitalText;
     Button CapitalA1, CapitalA2, CapitalA3, CapitalA4;
-    int i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
