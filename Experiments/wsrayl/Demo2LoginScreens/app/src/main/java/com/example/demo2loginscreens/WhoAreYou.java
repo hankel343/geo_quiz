06d834a1f16a594c0ee20b9b7800e17038e59af5
Wyatt Rayl <wsrayl@iastate.edu>
2023-10-08 14:22:02 -0500

    Added loginscreenfiles

---
+++ Experiments/wsrayl/Demo2LoginScreens/app/src/main/java/com/example/demo2loginscreens/WhoAreYou.java
@@ -0,0 +1,14 @@
+package com.example.demo2loginscreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class WhoAreYou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.who_are_you);
+    }
+}
