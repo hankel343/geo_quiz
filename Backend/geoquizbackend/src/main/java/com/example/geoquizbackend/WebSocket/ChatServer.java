6bdca90f89c640e3d187ab89345e2466631a0d86
hhaldin <haldin@iastate.edu>
2023-10-14 17:09:44 -0500

    Create class with imports and add dependency

---
+++ Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/WebSocket/ChatServer.java
@@ -0,0 +1,21 @@
+package com.example.geoquizbackend.WebSocket;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+public class ChatServer {
+
+}
