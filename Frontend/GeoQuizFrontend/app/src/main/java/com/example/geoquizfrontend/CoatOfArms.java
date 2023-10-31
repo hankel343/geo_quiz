0a7efff4ac467d31ef50ba923d8d134a89f0a14b
hhaldin <haldin@iastate.edu>
2023-10-30 20:02:09 -0500

    Successfully pull country names into front end

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CoatOfArms.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CoatOfArms.java
@@ -3,12 +3,27 @@ package com.example.geoquizfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.geoquizfrontend.models.GameData;
+import com.example.geoquizfrontend.services.CountryService;
+
+import java.util.List;
 
 public class CoatOfArms extends AppCompatActivity {
+    TextView GameText, ScoreText;
+    Button Opt0, Opt1, Opt2, Opt3;
 
+    private int rounds = 4;
+    private int score = 0;
+    private String[] countryNames;
+    private List<GameData> countryDataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coat_of_arms);
+        CountryService countryService = new CountryService(this);
+        countryNames = countryService.getCountryNames();
     }
 }
--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/services/CountryService.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/services/CountryService.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -16,9 +17,15 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+
 public class CountryService {
     private static final String COUNTRY_URL = "http://coms-309-012.class.las.iastate.edu:8080/countries";
     private final RequestQueue requestQueue;
+
+    private final String[] countryNames = new String[243];
 
     public CountryService(Context context) {
         requestQueue = Volley.newRequestQueue(context);
@@ -30,6 +37,34 @@ public class CountryService {
 
         // Add the request to the queue and execute it
         requestQueue.add(req);
+    }
+
+    public String[] getCountryNames() {
+        String url = COUNTRY_URL + "/testCommon";
+
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response;
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            countryNames[i] = jsonArray.getString(i);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace(); // Handle JSON parsing error
+                    }
+                },
+                error -> {
+                    error.printStackTrace(); // Handle network error
+                }
+        );
+
+        requestQueue.add(req);
+
+        // You may want to wait for the network request to complete here
+        // before returning the country names, which may require using callbacks
+        // or other mechanisms to ensure the data is available.
+
+        return countryNames;
     }
 
     public List<GameData> parseGameDataResponse(JSONArray response) {
