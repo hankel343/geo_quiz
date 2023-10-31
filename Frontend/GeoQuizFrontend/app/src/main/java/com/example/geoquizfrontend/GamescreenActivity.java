f13fdaf30ca069287b726900036f90d27f76c57d
hhaldin <haldin@iastate.edu>
2023-10-30 19:25:35 -0500

    Add routing for the Coat of Arms game

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/GamescreenActivity.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/GamescreenActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class GamescreenActivity extends AppCompatActivity {
-    Button flagGame, capitalGame, backBtn, continentsGame;
+    Button flagGame, capitalGame, backBtn, continentsGame, coatOfArmsGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +19,41 @@ public class GamescreenActivity extends AppCompatActivity {
         capitalGame = (Button) findViewById(R.id.CapitalGameB);
         flagGame = (Button) findViewById(R.id.FlagGameB);
         continentsGame = (Button) findViewById(R.id.continents_btn);
+        coatOfArmsGame = (Button) findViewById(R.id.coatOfArms_btn);
         backBtn = (Button) findViewById(R.id.back_btn);
 
         capitalGame.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(GamescreenActivity.this, CapitalGame.class);
-                startActivity(intent);
+                startActivity(
+                        new Intent(GamescreenActivity.this, CapitalGame.class)
+                );
             }
         });
         flagGame.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(GamescreenActivity.this, FlagGame.class);
-                startActivity(intent);
+                startActivity(
+                        new Intent(GamescreenActivity.this, FlagGame.class)
+                );
             }
         });
 
         continentsGame.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(GamescreenActivity.this, ContinentsQuiz.class);
-                startActivity(intent);
+                startActivity(
+                        new Intent(GamescreenActivity.this, ContinentsQuiz.class)
+                );
+            }
+        });
+
+        coatOfArmsGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(
+                        new Intent(GamescreenActivity.this, CoatOfArms.class)
+                );
             }
         });
 
