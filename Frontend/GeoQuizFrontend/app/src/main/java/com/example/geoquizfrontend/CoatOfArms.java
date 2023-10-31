db2adf2eca6ea5c43ad543708cdc55e033901521
hhaldin <haldin@iastate.edu>
2023-10-30 19:21:21 -0500

    Create layout for coat of arms game

---
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CoatOfArms.java
@@ -0,0 +1,14 @@
+package com.example.geoquizfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoatOfArms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coat_of_arms);
+    }
+}
