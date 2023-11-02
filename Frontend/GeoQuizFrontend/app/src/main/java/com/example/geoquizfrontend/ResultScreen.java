fe03a744e1ca3011f84e391eaa24554b6d5a50ad
hhaldin <haldin@iastate.edu>
2023-11-02 12:09:56 -0500

    Fix result screen routing

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/ResultScreen.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/ResultScreen.java
@@ -5,6 +5,7 @@ import static com.example.geoquizfrontend.ApiClientFactory.GetCapitalQuizApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,9 @@ public class ResultScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_screen);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("UserInfo", MODE_PRIVATE);
+
         YourScore = (TextView) findViewById(R.id.YourScore);
         DurationText = (TextView) findViewById(R.id.DurationText);
         SeeScoreB = (Button) findViewById(R.id.SeeScoreB);
@@ -47,8 +51,9 @@ public class ResultScreen extends AppCompatActivity {
         PlayAgainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ResultScreen.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(
+                        new Intent(new Intent(ResultScreen.this, GamescreenActivity.class)
+                ));
             }
         });
     }
