8c3e7d2e3180fca2ae97752e06db2db7a70c6129
Brayden Lamb <balamb@iastate.edu>
2023-11-02 18:38:10 -0500

    Population Game layout

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/PopulationHiLo.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/PopulationHiLo.java
@@ -3,12 +3,28 @@ package com.example.geoquizfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PopulationHiLo extends AppCompatActivity {
-
+    private TextView PopulationRules1, PopulationRules2, Countryone, PopulationOne, Countrytwo, Populationtwo, PopScore, HighPopScore;
+    private Button HigherBtn, LowerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_population_hi_lo);
+
+        PopulationRules1 = (TextView) findViewById(R.id.PopulationRules1);
+        PopulationRules2 = (TextView) findViewById(R.id.PopulationRules2);
+        Countryone = (TextView) findViewById(R.id.Countryone);
+        PopulationOne = (TextView) findViewById(R.id.PopulationOne);
+        Countrytwo = (TextView) findViewById(R.id.Countrytwo);
+        Populationtwo = (TextView) findViewById(R.id.Populationtwo);
+        PopScore = (TextView) findViewById(R.id.PopScore);
+        HighPopScore = (TextView) findViewById(R.id.HighPopScore);
+        HigherBtn = (Button) findViewById(R.id.HigherBtn);
+        LowerBtn = (Button) findViewById(R.id.LowerBtn);
+
+        
     }
 }
