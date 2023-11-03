5f5c5d1bef613aabe69e20fa2e4f31bc3bcbc9c1
Brayden Lamb <balamb@iastate.edu>
2023-11-02 19:14:59 -0500

    Population Game func

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/PopulationHiLo.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/PopulationHiLo.java
@@ -1,14 +1,34 @@
 package com.example.geoquizfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import static com.example.geoquizfrontend.ApiClientFactory.GetCapitalQuizApi;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.geoquizfrontend.models.GameData;
+import com.example.geoquizfrontend.models.CapitalQuiz;
+import com.example.geoquizfrontend.services.CountryService;
+import com.example.geoquizfrontend.services.RandomNumberGenerator;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PopulationHiLo extends AppCompatActivity {
     private TextView PopulationRules1, PopulationRules2, Countryone, PopulationOne, Countrytwo, Populationtwo, PopScore, HighPopScore;
     private Button HigherBtn, LowerBtn;
+    private int score = 0;
+    private int populationOne = 0;
+    private int populationTwo = 0;
+    private String countryOne = "broken";
+    private String countryTwo = "broken";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +45,52 @@ public class PopulationHiLo extends AppCompatActivity {
         HigherBtn = (Button) findViewById(R.id.HigherBtn);
         LowerBtn = (Button) findViewById(R.id.LowerBtn);
 
-        
+        //CapitalQuizApi apiService = ApiClientFactory.GetCapitalQuizApi();
+        //Call<List<GameData>> call = apiService.GetGameDataALL();
+
+//        call.enqueue(new Callback<List<GameData>>() {
+//            @Override
+//            public void onResponse(Call<List<GameData>> call, Response<List<GameData>> response) {
+//                if (response.isSuccessful()) {
+//                    countryDataList = response.body();
+//                    gameTick();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<GameData>> call, Throwable t) {
+//
+//            }
+//        });
+        HigherBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(populationOne >= populationTwo){
+                    Populationtwo.setTextColor(Color.GREEN);
+                    Countryone.setText(countryTwo);
+                    PopulationOne.setText(Integer.toString(populationTwo));
+                    updateScore();
+                }
+            }
+        });
+
+        LowerBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(populationOne <= populationTwo){
+                    Populationtwo.setTextColor(Color.GREEN);
+                    Countryone.setText(countryTwo);
+                    PopulationOne.setText(Integer.toString(populationTwo));
+                    updateScore();
+                }else{
+
+                }
+
+            }
+        });
+    }
+    private void updateScore() {
+        score += 1;
+        PopScore.setText(Integer.toString(score));
     }
 }
