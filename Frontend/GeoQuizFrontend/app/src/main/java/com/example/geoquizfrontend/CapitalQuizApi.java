a5fa1a7b5c2aaac110c3731e936ef92df15707cd
hhaldin <haldin@iastate.edu>
2023-11-02 20:07:04 -0500

    Alter front end to handle multiple objects returned

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalQuizApi.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalQuizApi.java
@@ -4,16 +4,19 @@ import com.example.geoquizfrontend.models.CapitalQuiz;
 import com.example.geoquizfrontend.models.GameData;
 import com.example.geoquizfrontend.models.Student;
 
+import java.util.ArrayList;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface CapitalQuizApi {
-    @GET("countries/gameDataOne")
-    Call<GameData> GetGameData();
+    @GET("countries/gameData")
+    Call<ArrayList<GameData>> GetGameData(@Query("count") int count);
     @PUT("quizzes/{id}")
     Call<CapitalQuiz> PutCapitalQuizByPath(@Path("id") int id, @Body CapitalQuiz newCapitalQuiz);
 
