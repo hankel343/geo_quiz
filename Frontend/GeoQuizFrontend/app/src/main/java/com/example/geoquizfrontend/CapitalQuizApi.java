59d119a763131f6bc79917b7081ffb607fa04fd1
hhaldin <haldin@iastate.edu>
2023-11-04 12:23:17 -0500

    Implemented student authentication

--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalQuizApi.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/CapitalQuizApi.java
@@ -15,17 +15,21 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface CapitalQuizApi {
+    // country data endpoints
     @GET("countries/gameData")
     Call<ArrayList<GameData>> GetGameData(@Query("count") int count);
+    // quiz endpoints
     @PUT("quizzes/{id}")
     Call<CapitalQuiz> PutCapitalQuizByPath(@Path("id") int id, @Body CapitalQuiz newCapitalQuiz);
-
     @POST("quizzes")
     Call<CapitalQuiz> PostCapitalQuizByBody(@Body CapitalQuiz newCapitalQuiz);
-
     @GET("quizzes/{id}")
     Call<CapitalQuiz> GetCapitalQuizByPath(@Path("id") String id);
-
+    // student endpoints
+    @GET("/students/exists")
+    Call<Boolean> existsByEmail(@Query("email") String email);
+    @GET("/students/authenticate")
+    Call<Student> authenticate(@Query("email") String email, @Query("password") String password);
     @POST("/students")
     Call<Student> postNewStudent(@Body Student newStudent);
 }
--- Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/StudentSignInActivity.java
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/StudentSignInActivity.java
@@ -3,11 +3,19 @@ package com.example.geoquizfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.geoquizfrontend.models.Student;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class StudentSignInActivity extends AppCompatActivity {
 
@@ -31,9 +39,62 @@ public class StudentSignInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = emailInput.getText().toString().trim();
                 String password = passwordInput.getText().toString().trim();
+
+                CapitalQuizApi api = ApiClientFactory.GetCapitalQuizApi();
+                Call<Boolean> call = api.existsByEmail(email);
+
+                call.enqueue(new Callback<Boolean>() {
+                    @Override
+                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                        if (response.isSuccessful()) {
+                            boolean exists = response.body();
+                            if (exists) {
+                                Call<Student> authenticateCall = api.authenticate(email, password);
+                                authenticateCall.enqueue(new Callback<Student>() {
+                                    @Override
+                                    public void onResponse(Call<Student> call, Response<Student> response) {
+                                        if (response.isSuccessful()) {
+                                            // The student's information is retrieved
+                                            Student student = response.body();
+
+                                            SharedPreferences sharedPreferences = getSharedPreferences("UserInfo", MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                                            editor.putLong("id", student.getId());
+                                            editor.putString("firstName", student.getFirstName());
+                                            editor.putString("lastName", student.getLastName());
+                                            editor.putString("email", student.getEmail());
+                                            editor.putString("classCode", student.getClassCode());
+                                            editor.putFloat("grade", student.getGrade());
+                                            editor.apply();
+
+                                            Toast.makeText(StudentSignInActivity.this, "Welcome, " + sharedPreferences.getString("firstName", ""), Toast.LENGTH_SHORT).show();
+                                            startActivity(
+                                                    new Intent(StudentSignInActivity.this, AccountHomeActivity.class)
+                                            );
+                                        } else {
+                                            // The student is not authenticated
+                                            Toast.makeText(StudentSignInActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<Student> call, Throwable t) {
+                                        // Handle the error
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(StudentSignInActivity.this, "No account associated with email: " + email, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Boolean> call, Throwable t) {
+                        // Handle the error
+                    }
+                });
             }
         });
-
         newUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
