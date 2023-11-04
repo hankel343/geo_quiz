ce2d54edc24977238f57cfb4f95ca4606a927245
hhaldin <haldin@iastate.edu>
2023-11-04 13:15:24 -0500

    Scaffold professor account creation screen

---
+++ Frontend/GeoQuizFrontend/app/src/main/java/com/example/geoquizfrontend/ProfessorAccountCreation.java
@@ -0,0 +1,14 @@
+package com.example.geoquizfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessorAccountCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_account_creation);
+    }
+}
