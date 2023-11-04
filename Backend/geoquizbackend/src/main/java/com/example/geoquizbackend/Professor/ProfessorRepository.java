7e40671b37265915cc7b5aa09bca208794c21ffe
hhaldin <haldin@iastate.edu>
2023-11-04 13:42:13 -0500

    Create professor repository and class

--- Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Professor/ProfessorRepository.java
+++ Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Professor/ProfessorRepository.java
@@ -1,11 +1,15 @@
 package com.example.geoquizbackend.Professor;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-public interface ProfessorRepository {
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
     Professor findById(long id);
-
+    Professor findByEmail(String email);
+    boolean existsByEmail(String email);
+    Professor findByEmailAndPassword(String email, String password);
     @Transactional
     void deleteById(long id);
 }
