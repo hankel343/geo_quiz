259b69af74492b13d2b6d59eab216b0b16f6d721
hhaldin <haldin@iastate.edu>
2023-11-04 21:48:54 -0500

    Create repository method and controller to return top N quiz scores

--- Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Quiz/QuizRepository.java
+++ Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Quiz/QuizRepository.java
@@ -1,5 +1,7 @@
 package com.example.geoquizbackend.Quiz;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,7 +9,7 @@ import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
     Quiz findById(long id);
-    List<Quiz> findTopNByOrderByScoreDesc(int n);
+    List<Quiz> findAllByOrderByScoreDesc();
     @Transactional
     void deleteById(long id);
 }
