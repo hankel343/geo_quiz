c58902ba8d04b503b527a77de9765c53ca32a644
hhaldin <haldin@iastate.edu>
2023-11-04 20:48:58 -0500

    Add necessary method to quiz repository

--- Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Quiz/Quiz.java
+++ Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Quiz/Quiz.java
@@ -7,37 +7,25 @@ import jakarta.persistence.*;
 
 @Entity
 public class Quiz {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
-
     private int score;
-
     private double duration;
-
      @ManyToOne
      @JsonIgnore
      private Student student;
-
     public Quiz() {
-
     }
-
     // =============================== Getters and Setters for each field ================================== //
-
     public long getId(){
         return id;
     }
     public void setId(long id){
         this.id = id;
     }
-
     public int getScore() { return score; }
     public void setScore(int score) { this.score = score; }
     public double getDuration() { return duration; }
     public void setDuration(double duration) { this.duration = duration; }
-
-
-
 }
--- Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Quiz/QuizRepository.java
+++ Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Quiz/QuizRepository.java
@@ -3,11 +3,11 @@ package com.example.geoquizbackend.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
     Quiz findById(long id);
-
-
-
+    List<Quiz> findAllByOrderByScoreDesc();
     @Transactional
     void deleteById(long id);
 }
