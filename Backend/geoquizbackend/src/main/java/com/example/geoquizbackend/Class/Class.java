6363bf471aaccf18a1aea161a8a8986b1b7985e5
hhaldin <haldin@iastate.edu>
2023-11-05 09:08:04 -0600

    Fix data structures in Class class

--- Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Class/Class.java
+++ Backend/geoquizbackend/src/main/java/com/example/geoquizbackend/Class/Class.java
@@ -4,13 +4,16 @@ import com.example.geoquizbackend.Professor.Professor;
 import com.example.geoquizbackend.Student.Student;
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 public class Class {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
     String name;
     @ManyToOne
+    @JoinColumn(name="professsor_id")
     Professor professor;
-    @ManyToMany
-    Student student;
+    @OneToMany(mappedBy = "class")
+    Set<Student> student;
 }
